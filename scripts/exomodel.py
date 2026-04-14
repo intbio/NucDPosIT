d9489fc816223df9875acf3847110891003a9d79
d-ryabov-biomsu <d.ryabov@intbio.org>
2026-04-14 16:03:53 +0300

    add exact lambda value argument

--- scripts/exomodel.py
+++ scripts/exomodel.py
@@ -61,9 +61,15 @@ def parse_arguments():
         default=1
     )
     parser.add_argument(
-    "--plot_fits",
-    help="plot each optimization results",
-    action="store_true",
+        "--plot_fits",
+        help="plot each optimization results",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--reg_coef",
+        help="fit with current regularization coeffizient value",
+        type=float,
+        default=None
     )
     return parser.parse_args()
 
@@ -77,26 +83,33 @@ def main():
 
     
     os.makedirs(args.output_dir, exist_ok=True)
-    
     loader = exo_model.TemplateLengthAnalyzer(args.bam_file)
     pairs, dist = loader.calculate_distribution()
-    model = exo_model.AdaptiveHistogramDeconvolution(dist, lamb_range=(args.start, args.end), n_jobs=args.njobs)
-    res = model.grid_search(n_lamb=args.npoints, verbose=args.verbose)
 
-    if not args.no_plots:  
-        outpath = os.path.join(args.output_dir, 'optimization_results.png')
-        fig, ax = model.plot_results()
-        fig.savefig(outpath)
-        plt.close(fig) 
+    if args.reg_coef is not None:
+        model = exo_model.HistogramDeconvolution(dist, args.reg_coef)
+        res = model.fit()
+        if not args.no_plots:  
+            outpath = os.path.join(args.output_dir, 'optimization_results.png')
+            fig, ax = model.plot()
+            fig.savefig(outpath)
+            plt.close(fig) 
+    else:
+        model = exo_model.AdaptiveHistogramDeconvolution(dist, lamb_range=(args.start, args.end), n_jobs=args.njobs)
+        res = model.grid_search(n_lamb=args.npoints, verbose=args.verbose)
+
+        if not args.no_plots:  
+            outpath = os.path.join(args.output_dir, 'optimization_results.png')
+            fig, ax = model.plot_results()
+            fig.savefig(outpath)
+            plt.close(fig) 
     
     errors_outpath = os.path.join(args.output_dir, 'errors.csv')
     model.save(errors_outpath)
 
-
-    plots_dir = os.path.join(args.output_dir, 'fit_plots')
-    os.makedirs(plots_dir, exist_ok=True)
-
     if args.plot_fits:
+        plots_dir = os.path.join(args.output_dir, 'fit_plots')
+        os.makedirs(plots_dir, exist_ok=True)
         figsize = (12, 5)  
         for i, row in model.get_summary_table().iterrows():
             result_x = row.result_x
